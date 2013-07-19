5e6c50a79c72eb9d5689a308c6b7682dc5e46bdd
andres18281 <andres18281@hotmail.com>
2013-07-19 09:08:50 -0700

    cambios nuevos

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(funtion(request,response){
-                  response.writeHead(200,{'Content-Type':'text/html'});
-                   var content = fs.readFileSync('index.html',utf8');
+                  response.writeHead(200,{'Content-Type':'text/plain'});
+                   var content = fs.readFileSync('index.html','UTF8');
                    response.end(content);
                  }).listen(1337);
                  console.log('Server running on 1337/');
