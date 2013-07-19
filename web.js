f37f5f88a427ba80a636443076facfdf1d71a36e
andres18281 <andres18281@hotmail.com>
2013-07-19 01:26:17 -0700

    cambio en web.js

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-var express = require('express');
+var http = require('http');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(funtion(request,response){
+                  response.writeHead(200,{'Content-Type':'text/plain'});
+                   var content = fs.readFileSync('index.html',utf8');
+                   response.end(content);
+                 }).listen(1337);
+                 console.log('Server running on 1337/');
